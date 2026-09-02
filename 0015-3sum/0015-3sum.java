6075884124e609f1cf9c6fb1943af68095c9cbcc
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-02 21:34:06 +0530

    Time: 28 ms (96.7%), Space: 59.3 MB (32.1%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,70 +2,39 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> result = new ArrayList<>();
-
-        // Step 1: Sort the array
-        Arrays.sort(nums);
-
-        // Step 2: Fix one element
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // If nums[i] > 0, sum can never be 0
-            if (nums[i] > 0) {
-                break;
-            }
-
-            // Skip duplicate fixed elements
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            // Step 3: Two pointers
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-
-                    // Found a valid triplet
-                    result.add(Arrays.asList(
-                        nums[i],
-                        nums[left],
-                        nums[right]
-                    ));
-
-                    // Move both pointers
+       List<List<Integer>> arr= new ArrayList<>();
+       Arrays.sort(nums);
+       for(int i=0;i< nums.length-2;i++){
+        if(nums[i]>0){
+            break;
+        }
+        if(i>0 && nums[i]== nums[i-1]){
+            continue;
+        }
+        int left=i+1;
+        int right= nums.length-1;
+        while(left<right){
+            int sum= nums[i]+ nums[left]+nums[right];
+            if(sum==0){
+                arr.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                left++;
+                right--;
+                while(left< right && nums[left]==nums[left-1]){
                     left++;
-                    right--;
-
-                    // Skip duplicate left values
-                    while (left < right &&
-                           nums[left] == nums[left - 1]) {
-                        left++;
-                    }
-
-                    // Skip duplicate right values
-                    while (left < right &&
-                           nums[right] == nums[right + 1]) {
-                        right--;
-                    }
-
-                } 
-                else if (sum < 0) {
-                    // Need a bigger sum
-                    left++;
-                } 
-                else {
-                    // Need a smaller sum
+                }
+                 while(left< right && nums[right]==nums[right+1]){
                     right--;
                 }
             }
-        }
+            else if( sum<0){
+                left++;
+            }
+            else{
+                right--;
+            }
 
-        return result;
+        }
+       }
+       return arr;
     }
 }
