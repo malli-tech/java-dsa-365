88498e3523b3aa318d7cde245934f62067bc217e
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-02 21:29:30 +0530

    Time: 30 ms (87.72%), Space: 59.1 MB (74.64%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -22,7 +22,7 @@ class Solution {
                 while(left< right && nums[left]==nums[left-1]){
                     left++;
                 }
-                 while(left< right && nums[right]==nums[left-1]){
+                 while(left< right && nums[right]==nums[right+1]){
                     right--;
                 }
             }
