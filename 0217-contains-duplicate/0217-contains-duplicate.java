ec2d0fe2206029932f864f8f534971cf47f83327
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-07 15:25:05 +0530

    Time: 108 ms (5.11%), Space: 109.4 MB (11.37%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,13 +2,11 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         TreeSet<Integer> set= new TreeSet<>();
         for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
             set.add(nums[i]);
         }
-        if(nums.length!=set.size()){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return false;
     }
 }
