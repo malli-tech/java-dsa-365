94b0e8427c9db3005b8e29fdfe6f933e61c45201
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-07 15:27:01 +0530

    Time: 18 ms (77.24%), Space: 108.1 MB (56.69%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
 
-        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
+        for (int num : nums) {
+            if (set.contains(num)) {
                 return true;
             }
+            set.add(num);
         }
 
         return false;
