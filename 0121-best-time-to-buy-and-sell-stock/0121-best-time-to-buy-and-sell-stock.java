e9fdee2180b81afe9b4f7b5e4a138426783bc4f5
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-09 23:54:21 +0530

    Time: 1 ms (99.95%), Space: 94.3 MB (70.75%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int p=prices[0];
+        int q=0;
+        for(int i=1;i<prices.length;i++){
+            if(p>prices[i]){
+                p=prices[i];
+                
+            }
+            else{
+                q=Math.max(q,prices[i]-p);
+            }
+        }
+        return q;
+    }
+}
