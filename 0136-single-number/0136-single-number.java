4fd967001ab5567e98641384a398f6df1fe89e8b
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-19 09:52:43 +0530

    Time: 1 ms (99.94%), Space: 47 MB (45.94%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,12 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-
-        for(int num : nums) {
-            result = result ^ num;
-        }
-
-        return result;
+      int r=0;
+      for(int num:nums){
+        r=r^num;
+      }
+      return r;
     
     }
 }
