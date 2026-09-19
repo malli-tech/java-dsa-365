75ecdae1a67cca615b6adee530dbc2028277766d
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-19 09:49:25 +0530

    Time: 1 ms (99.94%), Space: 46.9 MB (64.78%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,14 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer>map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
+        int result = 0;
+
+        for(int num : nums) {
+            result = result ^ num;
         }
-        for(int num:nums){
-            if(map.get(num)==1){
-                return num;
-            }
-        }
-        return 0;
+
+        return result;
+    
     }
 }
