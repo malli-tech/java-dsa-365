7f510e02e775d05b9a93b7432a290ced0025f473
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-20 00:37:56 +0530

    Time: 1 ms (77.7%), Space: 42.2 MB (88.49%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        while(n != 1) {
+
+            if(set.contains(n)) {
+                return false;
+            }
+
+            set.add(n);
+
+            int sum = 0;
+
+            while(n > 0) {
+                int rem = n % 10;
+                n = n / 10;
+
+                sum += rem * rem;
+            }
+
+            n = sum;
+        }
+
+        return true;
+    }
+}
