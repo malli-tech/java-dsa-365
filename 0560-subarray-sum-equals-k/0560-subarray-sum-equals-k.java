2be394e4ebc53db3fe71e2c17e75804cbc6a9d24
malli-tech <vennapusa.malli2024@vitstudent.ac.in>
2026-09-20 09:05:46 +0530

    Time: 24 ms (75.43%), Space: 48.9 MB (34.45%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,27 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-   HashMap<Integer, Integer> map = new HashMap<>();
-
-        // Prefix sum 0 has occurred once
-        map.put(0, 1);
-
-        int sum = 0;
-        int count = 0;
-
-        for (int num : nums) {
-
-            // Current prefix sum
-            sum += num;
-
-            // Check whether a previous prefix sum exists
-            if (map.containsKey(sum - k)) {
-                count += map.get(sum - k);
-            }
-
-            // Store/update the current prefix sum
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
-        }
-
-        return count;
+   HashMap<Integer, Integer>map=new HashMap<>();
+   map.put(0,1);
+   int count=0;
+   int sum=0;
+   for(int num:nums){
+      sum+=num;
+      if(map.containsKey(sum-k)){
+        count+=map.get(sum-k);
+      }
+      map.put(sum, map.getOrDefault(sum,0)+1);
+   }
+   return count;
     }
 }
